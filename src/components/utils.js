c75c2a09b0493793a11b4274666fe09785dc3ccb
VAISHNAVI GADHAVE <vgadhave411@gmail.com>
2023-08-14 10:36:16 +0530

    first commit

---
+++ src/components/utils.js
@@ -0,0 +1,12 @@
+
+export const paginate = (followers) => {
+    const itemsPerPage = 10;
+    const numberOfPages = Math.ceil(followers.length/ itemsPerPage);
+
+    //logic to generate new followers per page
+    const newFollowers = Array.from({length: numberOfPages}, (_,index)=>{
+        const start = index * itemsPerPage;
+        return followers.slice(start, start + itemsPerPage);
+    });
+    return newFollowers;
+}
